751e3e2143e0ce7345920171c5b8047f61a697ab
DuTomi <dudas.tamas.alex@gmail.com>
2023-01-25 16:15:10 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Dudas Tamas Alex on 2023. 01. 25..
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
